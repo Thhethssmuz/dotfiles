262ea6dbe19238c5177d6334ed61834fc2ee955a
Thhethssmuz <thhethssmuz@gmail.com>
2015-10-02 20:35:18 +0200

    pacapt

--- .scripts/apt.sh
+++ .scripts/apt.sh
@@ -2,14 +2,26 @@
 # apt simplifier
 #
 
-apt() {
+pacapt() {
   case "$1" in
-    -S)    apt-get install ${@:2}                       ;;
-    -Ss)   apt-cache search ${@:2}                      ;;
-    -Rs)   apt-get remove ${@:2}                        ;;
-    -Syu)  apt-get update && apt-get upgrade            ;;
-    -Q)    dpkg -l                                      ;;
-    # -Qs)
-    *)     echo "error: invalid option $1"              ;;
+    -S)    sudo apt-get install ${@:2} ;;
+    -Ss)   apt-cache search ${@:2} ;;
+    -Sy)   sudo apt-get update ;;
+    -Su)   sudo apt-get upgrade ;;
+    -Syu)  sudo apt-get update && sudo apt-get upgrade ;;
+
+    -Rs)   sudo apt-get remove ${@:2} ;;
+
+    -Q)    dpkg -l | awk '{print $2" "$3'} ;;
+    -Qs)   dpkg -l | \
+            grep $(printf '%q' "${@:2}") | \
+            awk '{ print $2" "$3; printf "   "; for(i=5;i<=NF;i++) printf " "$i; print "" }' ;;
+
+    -U)    for file in ${@:2}; do
+             sudo dpkg -i $file
+             sudo apt-get install -f
+           done
+
+    *)     echo "error: invalid option $1" ;;
   esac
 }
