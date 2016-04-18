457c149ff4ade5c40afb2d9fea95f1dcbbbe61b8
Thhethssmuz <thhethssmuz@gmail.com>
2016-04-18 22:23:42 +0200

    status menus

---
+++ .xmonad/lib/DzenMenu.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+close() {
+  dbus-send \
+    --type=signal \
+    --dest=org.DzenMenu.Server \
+    /org/DzenMenu/Server \
+    org.DzenMenu.Server.Close \
+    string:$1
+}
+
+trap "close $1" EXIT
+
+${@:2}
