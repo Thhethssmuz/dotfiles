3204bab345229179d82f75194757ff6d899dc99c
Thhethssmuz <thhethssmuz@gmail.com>
2016-08-01 22:40:31 +0200

    use prompts and custom swap

--- .install/live.sh
+++ .install/live.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-set -exuo pipefail
+set -eo pipefail
 
-abort() {
-  echo "$1" 1>&2
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be root inside live environment" 1>&2
   exit 1
-}
-[[ $EUID -eq 0 ]] || abort "You must be root inside live environment"
-[[ $DISK ]] || abort "You must specify the disk to partition"
-[[ $HOSTNAME ]] || abort "You must specify a hostname"
-
-echo "WARNING: this will overwrite the disk $DISK"
-echo "This is a $(lsblk "$DISK" -ldn | awk '{print $4}') disk"
-read -rp "Press enter to continue or ctrl+c to cancel: "
+fi
 
 loadkeys dvorak
 dhcpcd
 
+source live-prompt.sh
+ping google.com -c 1 || source ip-prompt.sh
+
+echo -e "\e[31mWARNING: this will overwrite the disk ${DISK}\e[0m"
+echo -e "\e[31mThis is a $(lsblk "$DISK" -ldn | awk '{print $4}') disk\e[0m"
+echo -e "\e[31mlast chance are you sure you want to do this?\e[0m"
+read -rp "Press enter to continue"
+
+# in case something goes wrong
+set -x
+
 # partition disk
-sgdisk -Z \
+sgdisk -Z "$DISK"
+sgdisk \
   -n 1:0:+512M -t 1:ef00 -c 1: bootefi \
   -n 2:0:0 -t 2:8300 -c 2:cryptlvm \
   -p "$DISK"
@@ -34,19 +39,25 @@ pvcreate /dev/mapper/lvm
 vgcreate vgroup /dev/mapper/lvm
 
 # create logical partitions on the volume group
-lvcreate -L 8G -n swap vgroup
+if [ -n "$SWAP" ]; then
+  lvcreate -L "$SWAP" -n swap vgroup
+fi
 lvcreate -l 100%free -n root vgroup
 
 # create filesystems
 mkfs.fat -F32 "${DISK}1"
 mkfs.ext4 /dev/mapper/vgroup-root
-mkswap /dev/mapper/vgroup-swap
+if [ -n "$SWAP" ]; then
+  mkswap /dev/mapper/vgroup-swap
+fi
 
 # mount filesystems
 mount /dev/mapper/vgroup-root /mnt
 mkdir -p /mnt/boot
 mount "${DISK}1" /mnt/boot
-swapon /dev/mapper/vgroup-swap
+if [ -n "$SWAP" ]; then
+  swapon /dev/mapper/vgroup-swap
+fi
 
 # base install
 pacstrap /mnt base base-devel
@@ -56,5 +67,8 @@ cp firstboot.sh /mnt/root
 arch-chroot /mnt ./chroot.sh "$DISK" "$HOSTNAME"
 
 umount -R /mnt
-swapoff /dev/mapper/vgroup-swap
-echo "chroot created, reboot and run firstboot.sh"
+if [ -n "$SWAP" ]; then
+  swapoff /dev/mapper/vgroup-swap
+fi
+
+echo "chroot created successfully, reboot and run firstboot.sh"
