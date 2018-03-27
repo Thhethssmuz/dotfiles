4f6864b22a49da75d7a2ca63f38014346bbec699
Thhethssmuz <thhethssmuz@gmail.com>
2018-03-27 15:36:33 +0200

    ignore groups that have no members

--- .install/lib/pac.sh
+++ .install/lib/pac.sh
@@ -6,7 +6,7 @@ list() {
 }
 
 ignore() {
-  pacman -Qgq base base-devel xorg xorg-drivers xorg-apps | sed 's/$/.pac/'
+  pacman -Qgq base base-devel xorg xorg-drivers xorg-apps 2>/dev/null | sed 's/$/.pac/'
 }
 
 
