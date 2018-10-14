9b2308f20bdbc878637480e4c3d0fee4ee8ace8a
Thhethssmuz <thhethssmuz@gmail.com>
2018-10-14 08:19:41 +0200

    console window

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -14,7 +14,9 @@ import XMonad.Layout.SubLayouts
 import XMonad.Layout.Tabbed
 import XMonad.Layout.WindowNavigation
 
+import XMonad.Actions.DynamicWorkspaces (addHiddenWorkspace)
 import XMonad.Actions.PhysicalScreens
+import XMonad.Actions.TagWindows
 
 import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks, docksEventHook, ToggleStruts(..))
 import XMonad.Hooks.DynamicLog
@@ -44,6 +46,7 @@ import Data.Bits ((.|.))
 import Data.Char (isAlphaNum)
 import Data.List
 import Data.Maybe
+import Data.Ratio
 import Data.Monoid
 import Control.Monad
 import Control.Applicative ((<$>))
@@ -312,14 +315,16 @@ myDzenMenus home notifd =
 -- Manage hooks
 -------------------------------------------------------------------------------
 
-myManageHook = (composeAll
-  [ resource  =? "dzen2"       --> doIgnore
-  , resource  =? "feh"         --> doIgnore
-  , resource  =? "guake"       --> doFloat
-  , className =? "stalonetray" --> doIgnore
-  --, isSticky                   --> doIgnore
-  , manageDocks
-  ]) <+> manageHook def
+myManageHook
+  =   (composeAll
+      [ resource  =? "dzen2"       --> doIgnore
+      , resource  =? "feh"         --> doIgnore
+      , resource  =? "guake"       --> doFloat
+      , className =? "stalonetray" --> doIgnore
+      --, isSticky                   --> doIgnore
+      , manageDocks
+      ])
+  <+> manageHook def
   --where
   --  isSticky = isInProperty "_NET_WM_STATE" "_NET_WM_STATE_STICKY"
 
@@ -407,6 +412,91 @@ myXPConf = def
   }
 
 -------------------------------------------------------------------------------
+-- Console
+-------------------------------------------------------------------------------
+
+consoleHide :: Window -> X ()
+consoleHide w = withWindowSet $ \s -> do
+  when (null . filter ((==) "_" . W.tag) . W.workspaces $ s) $
+    addHiddenWorkspace "_"
+  focusPrevious w
+  windows . W.shiftWin "_" $ w
+
+consoleShow :: Window -> X ()
+consoleShow w = withWindowSet $ \s -> do
+  let p  = foldl1 (\x y -> if W.screen x < W.screen y then x else y)
+         $ W.current s : W.visible s
+      h  = fromIntegral . rect_height . screenRect $ W.screenDetail p
+      r  = W.RationalRect 0 (22 % h) 1 0.5
+      ws = W.integrate' $ W.stack =<< find ((==) "_" . W.tag) (W.workspaces s)
+
+  when (maybe False ((==) $ length ws - 1) $ elemIndex w ws) $
+    runOnWorkspaces $ \ws -> if W.tag ws /= "_" then return ws else
+      return $ ws { W.stack = fmap W.focusDown' $ W.stack ws }
+
+  windows $ W.view (W.tag $ W.workspace p)
+          . W.shiftWin (W.tag $ W.workspace p) w
+          . W.float w r
+
+consoleToggle :: X ()
+consoleToggle = withWindowSet $ \s -> do
+  vs <- filterM (hasTag "console") . maybe [] W.integrate . W.stack
+      . W.workspace . foldl1 (\x y -> if W.screen x < W.screen y then x else y)
+      $ W.current s : W.visible s
+  xs <- filterM (hasTag "console") $ W.allWindows s
+
+  mapM_ consoleHide $ xs
+  if not (null vs)
+    then mapM_ consoleHide vs
+    else case W.stack =<< find ((==) "_" . W.tag) (W.workspaces s) of
+      Just ws -> consoleShow $ W.focus ws
+      _       -> return ()
+
+consoleNext :: X ()
+consoleNext = withWindowSet $ \s -> do
+  vs <- filterM (hasTag "console") . maybe [] W.integrate . W.stack
+      . W.workspace . foldl1 (\x y -> if W.screen x < W.screen y then x else y)
+      $ W.current s : W.visible s
+  xs <- filterM (hasTag "console") $ W.allWindows s
+  mapM_ consoleHide $ xs ++ vs
+  runOnWorkspaces $ \ws -> if W.tag ws /= "_" then return ws else do
+    return $ ws { W.stack = fmap W.focusUp' $ W.stack ws }
+  consoleToggle
+
+consolePrev :: X ()
+consolePrev = withWindowSet $ \s -> do
+  vs <- filterM (hasTag "console") . maybe [] W.integrate . W.stack
+      . W.workspace . foldl1 (\x y -> if W.screen x < W.screen y then x else y)
+      $ W.current s : W.visible s
+  xs <- filterM (hasTag "console") $ W.allWindows s
+  mapM_ consoleHide $ xs ++ vs
+  runOnWorkspaces $ \ws -> if W.tag ws /= "_" then return ws else do
+    return $ ws { W.stack = fmap W.focusDown' $ W.stack ws }
+  consoleToggle
+
+consoleOrGroupNext :: X ()
+consoleOrGroupNext = withFocused $ \w -> do
+  g <- hasTag "console" w
+  if g
+    then consoleNext
+    else onGroup W.focusDown'
+
+consoleOrGroupPrev :: X ()
+consoleOrGroupPrev = withFocused $ \w -> do
+  g <- hasTag "console" w
+  if g
+    then consolePrev
+    else onGroup W.focusUp'
+
+consoleInsert :: X ()
+consoleInsert = withFocused $ \w -> do
+  addTag "console" w
+  consoleHide w
+
+consoleRelease :: X ()
+consoleRelease = withFocused unTag
+
+-------------------------------------------------------------------------------
 -- Key Bindings
 -------------------------------------------------------------------------------
 
@@ -445,10 +535,8 @@ myKeys home conf@(XConfig { modMask = modMask }) =
   , ((modMask,                  xK_Return ), windows W.swapMaster)
   , ((modMask,                  xK_minus  ), sendMessage Shrink)
   , ((modMask,                  xK_plus   ), sendMessage Expand)
-  , ((modMask,                  xK_t      ), withFocused $ windows . W.sink)
-  -- , ((modMask,                   ), sendMessage $ IncMasterN 1)
-  -- , ((modMask,                   ), sendMessage $ IncMasterN (-1))
-
+  , ((modMask,                  xK_t      ), consoleRelease >> withFocused (windows . W.sink))
+  , ((modMask .|. shiftMask,    xK_t      ), consoleInsert)
 
   -- set focus to what ever workspace is visible on screen n
   , ((modMask,                  xK_1      ), viewScreen def 0)
@@ -461,40 +549,42 @@ myKeys home conf@(XConfig { modMask = modMask }) =
   , ((modMask .|. shiftMask,    xK_3      ), sendToScreen def 2)
 
   -- set focus to workspace n
-  , ((modMask,                   xK_aring ), windows . W.greedyView $ workspaces conf !! 0)
-  , ((modMask,                   xK_comma ), windows . W.greedyView $ workspaces conf !! 1)
-  , ((modMask,                   xK_period), windows . W.greedyView $ workspaces conf !! 2)
-  , ((modMask,                   xK_a     ), windows . W.greedyView $ workspaces conf !! 3)
-  , ((modMask,                   xK_o     ), windows . W.greedyView $ workspaces conf !! 4)
-  , ((modMask,                   xK_e     ), windows . W.greedyView $ workspaces conf !! 5)
-  , ((modMask,                   xK_ae    ), windows . W.greedyView $ workspaces conf !! 6)
-  , ((modMask,                   xK_q     ), windows . W.greedyView $ workspaces conf !! 7)
-  , ((modMask,                   xK_j     ), windows . W.greedyView $ workspaces conf !! 8)
+  , ((modMask,                  xK_aring ), windows . W.greedyView $ workspaces conf !! 0)
+  , ((modMask,                  xK_comma ), windows . W.greedyView $ workspaces conf !! 1)
+  , ((modMask,                  xK_period), windows . W.greedyView $ workspaces conf !! 2)
+  , ((modMask,                  xK_a     ), windows . W.greedyView $ workspaces conf !! 3)
+  , ((modMask,                  xK_o     ), windows . W.greedyView $ workspaces conf !! 4)
+  , ((modMask,                  xK_e     ), windows . W.greedyView $ workspaces conf !! 5)
+  , ((modMask,                  xK_ae    ), windows . W.greedyView $ workspaces conf !! 6)
+  , ((modMask,                  xK_q     ), windows . W.greedyView $ workspaces conf !! 7)
+  , ((modMask,                  xK_j     ), windows . W.greedyView $ workspaces conf !! 8)
 
   -- move window to workspace n
-  , ((modMask .|. shiftMask,     xK_aring ), windows . W.shift      $ workspaces conf !! 0)
-  , ((modMask .|. shiftMask,     xK_comma ), windows . W.shift      $ workspaces conf !! 1)
-  , ((modMask .|. shiftMask,     xK_period), windows . W.shift      $ workspaces conf !! 2)
-  , ((modMask .|. shiftMask,     xK_a     ), windows . W.shift      $ workspaces conf !! 3)
-  , ((modMask .|. shiftMask,     xK_o     ), windows . W.shift      $ workspaces conf !! 4)
-  , ((modMask .|. shiftMask,     xK_e     ), windows . W.shift      $ workspaces conf !! 5)
-  , ((modMask .|. shiftMask,     xK_ae    ), windows . W.shift      $ workspaces conf !! 6)
-  , ((modMask .|. shiftMask,     xK_q     ), windows . W.shift      $ workspaces conf !! 7)
-  , ((modMask .|. shiftMask,     xK_j     ), windows . W.shift      $ workspaces conf !! 8)
-
+  , ((modMask .|. shiftMask,    xK_aring ), windows . W.shift $ workspaces conf !! 0)
+  , ((modMask .|. shiftMask,    xK_comma ), windows . W.shift $ workspaces conf !! 1)
+  , ((modMask .|. shiftMask,    xK_period), windows . W.shift $ workspaces conf !! 2)
+  , ((modMask .|. shiftMask,    xK_a     ), windows . W.shift $ workspaces conf !! 3)
+  , ((modMask .|. shiftMask,    xK_o     ), windows . W.shift $ workspaces conf !! 4)
+  , ((modMask .|. shiftMask,    xK_e     ), windows . W.shift $ workspaces conf !! 5)
+  , ((modMask .|. shiftMask,    xK_ae    ), windows . W.shift $ workspaces conf !! 6)
+  , ((modMask .|. shiftMask,    xK_q     ), windows . W.shift $ workspaces conf !! 7)
+  , ((modMask .|. shiftMask,    xK_j     ), windows . W.shift $ workspaces conf !! 8)
 
   -- guake
-  , ((0,                         xK_bar   ), spawn "guake -t")
+  -- , ((0,                        xK_bar), spawn "guake -t")
+  , ((0,                        xK_bar), consoleToggle)
+  , ((mod1Mask,                 xK_bar), consoleOrGroupNext)
+  , ((mod1Mask .|. shiftMask,   xK_bar), consoleOrGroupPrev)
 
   -- screen lock
-  , ((mod1Mask .|. controlMask,  xK_l     ), spawn $ home ++ ".xmonad/scripts/system.sh --lock")
-  , ((modMask .|. shiftMask,     xK_F12   ), spawn (home ++ ".xmonad/scripts/system.sh --logout") >> io (exitWith ExitSuccess))
-  , ((modMask,                   xK_F5    ), spawn $ home ++ ".xmonad/scripts/system.sh --rexmonad")
-  , ((modMask,                   xK_F6    ), sendMessage ToggleStruts)
+  , ((mod1Mask .|. controlMask, xK_l     ), spawn $ home ++ ".xmonad/scripts/system.sh --lock")
+  , ((modMask .|. shiftMask,    xK_F12   ), spawn (home ++ ".xmonad/scripts/system.sh --logout") >> io (exitWith ExitSuccess))
+  , ((modMask,                  xK_F5    ), spawn $ home ++ ".xmonad/scripts/system.sh --rexmonad")
+  , ((modMask,                  xK_F6    ), sendMessage ToggleStruts)
 
   -- print screen
-  , ((0,                         xK_Print ), spawn $ home ++ ".xmonad/scripts/screenshot.sh")
-  , ((shiftMask,                 xK_Print ), spawn $ home ++ ".xmonad/scripts/screenshot.sh -u")
+  , ((0,                        xK_Print ), spawn $ home ++ ".xmonad/scripts/screenshot.sh")
+  , ((shiftMask,                xK_Print ), spawn $ home ++ ".xmonad/scripts/screenshot.sh -u")
 
   -- volume control
   , ((0,                        0x1008FF12), spawn $ home ++ ".xmonad/scripts/volume.sh -m")
