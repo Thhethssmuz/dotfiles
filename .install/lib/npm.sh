a686d52d684b0ed10587021c93a1c8f17ce1f797
Thhethssmuz <thhethssmuz@gmail.com>
2019-10-29 09:44:42 +0100

    npm .local

--- .install/lib/npm.sh
+++ .install/lib/npm.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 set -euo pipefail
 
+: "${USERNAME:=$USER}"
+: "${USERHOME:=$(sudo -Hu "$USERNAME" -s -- echo '$HOME')}"
+
+
 list() {
   if [ -d /usr/lib/node_modules ]; then
-    find /usr/lib/node_modules -mindepth 1 -maxdepth 1 -type d -print0 | \
+    find "$USERHOME"/.local/lib/node_modules -mindepth 1 -maxdepth 1 -type d -print0 | \
       xargs --null basename -a | sed 's/$/\.npm/'
   fi
 }
 
 ignore() {
-  echo $'node-gyp.npm\nnpm.npm\nrebuild.npm\nsemver.npm'
+  :
 }
 
 
 check-updates() {
-  (npm outdated -g --depth=0 2>/dev/null || true) | tail -n+2 | \
+  (npm outdated -g --depth=0 2>/dev/null || :) | tail -n+2 | \
     awk '{print "npm " $1 " " $2 " -> " $4}'
 }
 
 update() {
-  (npm outdated -g --depth=0 --parseable 2>/dev/null || true) | cut -d: -f4 | \
-    xargs --no-run-if-empty sudo npm 'install' -g
+  (npm outdated -g --depth=0 --parseable 2>/dev/null || :) | cut -d: -f4 | \
+    xargs --no-run-if-empty npm -g 'install'
 }
 
 
 install() {
-  sed 's/\.npm$//' | xargs --no-run-if-empty \
-    sudo npm 'install' -g
+  sed 's/\.npm$//' | xargs --no-run-if-empty npm -g 'install'
 }
 
 explicit() {
@@ -34,8 +37,7 @@ explicit() {
 }
 
 remove() {
-  sed 's/\.npm$//' | xargs --no-run-if-empty \
-    sudo npm uninstall -g
+  sed 's/\.npm$//' | xargs --no-run-if-empty npm -g uninstall
 }
 
 
