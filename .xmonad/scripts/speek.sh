e847af180acf9db69b8b4d92f158843360435750
Thhethssmuz <thhethssmuz@gmail.com>
2022-03-22 00:08:01 +0100

    fix dyslexia

---
+++ .xmonad/scripts/speek.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+ESPEED=320
+ELANG=en
+TLANG=eng
+
+speek() {
+  local selected
+  selected="$(xsel && echo)"
+
+  if [ -z "$selected" ]; then
+
+    scrot -s - 2>/dev/null | \
+      tesseract -l "$TLANG" - - 2>/dev/null | \
+      espeak --stdin -s "$ESPEED" -v "$ELANG"
+
+  else
+
+    espeak --stdin -s "$ESPEED" -v "$ELANG" <<< "$selected"
+
+  fi
+}
+
+stop() {
+  pkill -9 espeak
+}
+
+set-language() {
+  case "$1" in
+
+    en)
+      ELANG=en
+      TLANG=eng
+      ;;
+
+    no)
+      ELANG=nb
+      TLANG=nor
+      ;;
+
+    *)
+      echo "$(basename "$0"): invalid option \`$1'" 1>&2
+      echo "Try $(basename "$0") --help for more info" 1>&2
+      exit 1
+      ;;
+
+  esac
+}
+
+usage() {
+  cat <<EOF
+Usage:
+  $(basename "$0") (-h | --help)
+  $(basename "$0") [-l <language>] <cmd>
+
+COMMAND
+  speek
+  stop
+
+OPTIONS:
+  -l, --lang <language>    select language, i.e. "en" or "no"
+  -h, --help               display this help and exit
+
+EOF
+}
+
+main() {
+  local cmd
+
+  while [[ $# -gt 0 ]]; do
+
+    case "$1" in
+
+      --lang=*)
+        set-language "${1#*=}"
+        ;;
+      -l|--lang)
+        shift
+        set-language "$1"
+        ;;
+      -l*)
+        set-language "${1:2}"
+        ;;
+
+      -h|--help)
+        usage
+        exit 0
+        ;;
+
+      --*)
+        echo "$(basename "$0"): invalid option \`$1'" 1>&2
+        echo "Try $(basename "$0") --help for more info" 1>&2
+        exit 1
+        ;;
+
+      -??*)
+        set -- "-${1:1:1}" "-${1:2}" "${@:2}"
+        continue
+        ;;
+
+      -*)
+        echo "$(basename "$0"): invalid option \`$1'" 1>&2
+        echo "Try $(basename "$0") --help for more info" 1>&2
+        exit 1
+        ;;
+
+      speek)
+        cmd=speek
+        ;;
+
+      stop)
+        cmd=stop
+        ;;
+
+      *)
+        echo "$(basename "$0"): invalid command \`$1'" 1>&2
+        echo "Try $(basename "$0") --help for more info" 1>&2
+        exit 1
+        ;;
+
+    esac
+
+    shift
+
+  done
+
+  if [ -z "$cmd" ]; then
+    echo "$(basename "$0"): missing command"
+    echo "Try $(basename "$0") --help for more info"
+    exit 1
+  fi
+
+  "$cmd"
+}
+
+main "$@"
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -609,9 +609,9 @@ myKeys home conf@(XConfig { modMask = modMask }) =
   , ((0,                        0x1008ff17), spawn "mpc next")
 
   -- espeak
-  , ((0,                        xK_F9     ), spawn "(xsel && echo) | espeak --stdin -s320")
-  , ((shiftMask,                xK_F9     ), spawn "(xsel && echo) | espeak --stdin -vnb -s320")
-  , ((0,                        xK_F10    ), spawn "pkill -9 espeak")
+  , ((0,                        xK_F9     ), spawn $ home ++ ".xmonad/scripts/speek.sh speek")
+  , ((shiftMask,                xK_F9     ), spawn $ home ++ ".xmonad/scripts/speek.sh -lno speek")
+  , ((0,                        xK_F10    ), spawn $ home ++ ".xmonad/scripts/speek.sh stop")
 
   ]
 
